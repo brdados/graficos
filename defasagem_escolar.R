6ad90738ae0e2d04bcfc9b57a4f19a7946c9f85a
Rafael N. Magalhães <rnunesmagalhaes@gmail.com>
2018-10-19 12:33:59 -0300

    Padronização do pontilhado

--- defasagem_escolar.R
+++ defasagem_escolar.R
@@ -23,17 +23,34 @@ p <- bd %>%
   ggplot(aes(x = ano, y = defasagem)) +
   geom_line(size = .98, alpha = 0.9) +
   scale_x_continuous(breaks = seq(1981, 2014, by = 3)) +
-  labs(color = NULL, x = "Ano", y = "Defasagem (em anos)",
+  labs(color = NULL, x = NULL, y = "Defasagem (em anos)",
        caption = "\nFonte: Ipeadata (goo.gl/a2hTsF)",
        title = "Defasagem Escolar Média - 10 a 14 anos",
        subtitle = "Diferença entre a escolaridade esperada para  uma criança e a atingida") +
   tema_brdados() +
-  geom_rect(aes(xmin = 1981, xmax = 1988, ymin = -Inf, ymax = Inf), alpha = .005) +
-  geom_rect(aes(xmin = 1995, xmax = 2002, ymin = -Inf, ymax = Inf), alpha = .005) +
-  geom_rect(aes(xmin = 2011, xmax = 2014, ymin = -Inf, ymax = Inf), alpha = .005) +
-  annotate("text", x = 1984.5, y = .9, label = "bold(Pré-Constituição)", parse = TRUE) +
-  annotate("text", x = 1998.5, y = 2.6, label = "bold(FHC)", parse = TRUE) +
-  annotate("text", x = 2006.5, y = 2.6, label = "bold(Lula)", parse = TRUE) +
+  geom_vline(xintercept = 1988, 
+             size = 0.98, 
+             colour = "#8c510a", 
+             alpha = 0.9, 
+             linetype = "dashed") +
+  geom_vline(xintercept = 1995,
+             size = 0.98, 
+             colour = "#8c510a", 
+             alpha = 0.9, 
+             linetype = "dashed") +
+  geom_vline(xintercept = 2003, 
+             size = 0.98, 
+             colour = "#8c510a", 
+             alpha = 0.9, 
+             linetype = "dashed") +
+  geom_vline(xintercept = 2011, 
+             size = 0.98, 
+             colour = "#8c510a", 
+             alpha = 0.9, 
+             linetype = "dashed") +
+  annotate("text", x = 1990.3, y = 2.6, label = "bold(Constituição)", parse = TRUE) +
+  annotate("text", x = 1996.2, y = 2.6, label = "bold(FHC)", parse = TRUE) +
+  annotate("text", x = 2004.2, y = 2.6, label = "bold(Lula)", parse = TRUE) +
   annotate("text", x = 2012.5, y = 2.6, label = "bold(Dilma)", parse = TRUE)
 
 # Exportação
