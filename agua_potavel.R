79d4a7209a0eba997590ab0a09ea0634e5e8c474
Rafael N. Magalhães <rnunesmagalhaes@gmail.com>
2018-10-20 15:49:47 -0300

    Correção de subtítuo e gráfico regional

--- agua_potavel.R
+++ agua_potavel.R
@@ -13,7 +13,7 @@ here() # dir de trabalho
 bd <- ipeadata("SPAGUA", type = "tibble")
 
 # Gráfico Brasil
-p <- bd %>%
+p_br <- bd %>%
         filter(NIVNOME == "Brasil") %>% 
         rename(ano = ANO) %>%
         mutate(cob_agua = VALVALOR * 100) %>% 
@@ -24,7 +24,7 @@ p <- bd %>%
         labs(color = NULL, x = NULL, y = "Acesso a água potável (%)",
              caption = "\nFonte: Ipeadata (goo.gl/PkFfMw)",
              title = "Acesso a Água Potável",
-             subtitle = "Percentual de domicílios com água potável na rede geral") +
+             subtitle = "Percentual de domicílios com água potável") +
         tema_brdados() +
         geom_vline(xintercept = 1988, 
                    size = 0.98, 
@@ -41,12 +41,51 @@ p <- bd %>%
                    colour = "#8c510a", 
                    alpha = 0.9, 
                    linetype = "dashed") +
-        annotate("text", x = 1985.9, y = 97, label = "Constituição", parse = FALSE) +
-        annotate("text", x = 1991.8, y = 97, label = "Lei do SUS", parse = FALSE) +
-        annotate("text", x = 2010.3, y = 92, label = "Diretrizes Nacionais \npara o Saneamento \nBásico", parse = FALSE)
-        
+        annotate("text", x = 1985.9, y = 5, label = "Constituição", parse = FALSE) +
+        annotate("text", x = 1991.9, y = 5, label = "Lei do SUS", parse = FALSE) +
+        annotate("text", x = 2010.3, y = 7, label = "Diretrizes Nacionais \npara o Saneamento \nBásico", parse = FALSE)
+ 
+# Gráfico Regiões
+p_reg <- bd %>%
+        filter(NIVNOME == "Regiões") %>% 
+        rename(ano = ANO) %>%
+        mutate(cob_agua = VALVALOR * 100) %>% 
+        ggplot(aes(x = ano, y = cob_agua,  group = TERNOME, color = TERNOME)) +
+        geom_line(size = .98, alpha = 0.9) +
+        scale_x_continuous(breaks = seq(1981, 2014, by = 3)) +
+        scale_y_continuous(breaks = seq(20, 100, by = 20), limits = c(0, 100)) +
+        labs(color = NULL, x = NULL, y = "Acesso a água potável (%)",
+             caption = "\nFonte: Ipeadata (goo.gl/PkFfMw)",
+             title = "Acesso a Água Potável",
+             subtitle = "Percentual de domicílios com água potável") +
+        tema_brdados() +
+        geom_vline(xintercept = 1988, 
+                   size = 0.98, 
+                   colour = "#8c510a", 
+                   alpha = 0.9, 
+                   linetype = "dashed") +
+        geom_vline(xintercept = 1990,
+                   size = 0.98, 
+                   colour = "#8c510a", 
+                   alpha = 0.9, 
+                   linetype = "dashed") +
+        geom_vline(xintercept = 2007, 
+                   size = 0.98, 
+                   colour = "#8c510a", 
+                   alpha = 0.9, 
+                   linetype = "dashed") +
+        annotate("text", x = 1985.9, y = 5, label = "Constituição", parse = FALSE) +
+        annotate("text", x = 1991.9, y = 5, label = "Lei do SUS", parse = FALSE) +
+        annotate("text", x = 2010.3, y = 7, label = "Diretrizes Nacionais \npara o Saneamento \nBásico", parse = FALSE)
+       
 # Exportação
-ggsave("agua_potavel.png", p, 
+ggsave("agua_potavel_br.png", p_br, 
+       width = 10, 
+       height = 7, 
+       units = "in", 
+       type = "cairo-png")
+
+ggsave("agua_potavel_reg.png", p_reg, 
        width = 10, 
        height = 7, 
        units = "in", 
