b69f21b2d20c093719cd646653a6f6902af9b726
Rafael N. Magalhães <rnunesmagalhaes@gmail.com>
2018-10-20 08:59:42 -0300

    Ajustes finais

--- defasagem_escolar.R
+++ defasagem_escolar.R
@@ -19,14 +19,13 @@ p <- bd %>%
   filter(NIVNOME == "Brasil") %>% 
   rename(ano = ANO,
          defasagem = VALVALOR) %>% 
-  #mutate(ano = as.numeric(ANO)) %>% 
   ggplot(aes(x = ano, y = defasagem)) +
   geom_line(size = .98, alpha = 0.9) +
   scale_x_continuous(breaks = seq(1981, 2014, by = 3)) +
   labs(color = NULL, x = NULL, y = "Defasagem (em anos)",
        caption = "\nFonte: Ipeadata (goo.gl/a2hTsF)",
        title = "Defasagem Escolar Média - 10 a 14 anos",
-       subtitle = "Diferença entre a escolaridade esperada para  uma criança e a atingida") +
+       subtitle = "Diferença entre a escolaridade esperada para uma criança e a atingida") +
   tema_brdados() +
   geom_vline(xintercept = 1988, 
              size = 0.98, 
@@ -49,9 +48,9 @@ p <- bd %>%
              alpha = 0.9, 
              linetype = "dashed") +
   annotate("text", x = 1990.3, y = 2.6, label = "bold(Constituição)", parse = TRUE) +
-  annotate("text", x = 1996.2, y = 2.6, label = "bold(FHC)", parse = TRUE) +
-  annotate("text", x = 2004.2, y = 2.6, label = "bold(Lula)", parse = TRUE) +
-  annotate("text", x = 2012.5, y = 2.6, label = "bold(Dilma)", parse = TRUE)
+  annotate("text", x = 1996, y = 2.6, label = "bold(FHC)", parse = TRUE) +
+  annotate("text", x = 2004, y = 2.6, label = "bold(Lula)", parse = TRUE) +
+  annotate("text", x = 2012.2, y = 2.6, label = "bold(Dilma)", parse = TRUE)
 
 # Exportação
 ggsave(plot = p, "defasagem_escolar.png", type = "cairo-png")
