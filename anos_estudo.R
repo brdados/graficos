2331ec878610e866d66ec8fe4df6601b6c568bee
Rafael N. Magalhães <rnunesmagalhaes@gmail.com>
2018-10-22 09:25:58 -0300

    Anos de estudo por sexo

--- anos_estudo.R
+++ anos_estudo.R
@@ -84,6 +84,36 @@ p_reg <- bd %>%
         annotate("text", x = 2004, y = 1, label = "bold(Lula)", parse = TRUE) +
         annotate("text", x = 2012.2, y = 1, label = "bold(Dilma)", parse = TRUE)
 
+# Gráfico Sexo ----
+p_sex <- bd %>% 
+        gather("Sexo", "Anos_estudo", 8:9) %>% 
+        ggplot(aes(x = Ano, y = Anos_estudo, group = Sexo, color = Sexo)) +
+        geom_line(size = .98, alpha = 0.9) +
+        scale_x_continuous(breaks = seq(1976, 2015, by = 3)) +
+        labs(color = NULL, x = NULL, y = "Anos de Estudo (Média)",
+             caption = "\nFonte: Elaboração própria a partir de dados da PNAD - IBGE",
+             title = "Anos de Estudo",
+             subtitle = "Média de anos de estudo da população brasileira com mais de 14 anos") +
+        tema_brdados() +
+        geom_vline(xintercept = 1988, 
+                   size = 0.98, 
+                   colour = "#8c510a", 
+                   alpha = 0.9, 
+                   linetype = "dashed") +
+        geom_vline(xintercept = 1996,
+                   size = 0.98, 
+                   colour = "#8c510a", 
+                   alpha = 0.9, 
+                   linetype = "dashed") +
+        geom_vline(xintercept = 2006, 
+                   size = 0.98, 
+                   colour = "#8c510a", 
+                   alpha = 0.9, 
+                   linetype = "dashed") +
+        annotate("text", x = 1990.5, y = 1, label = "bold(Constituição)", parse = TRUE) +
+        annotate("text", x = 1997.8, y = 1, label = "bold(FUNDEF)", parse = TRUE) +
+        annotate("text", x = 2007.8, y = 1, label = "bold(FUNDEB)", parse = TRUE)
+
 # Exportação
 ggsave("anos_estudo_br.png", p_br, 
        width = 10, 
@@ -92,6 +122,12 @@ ggsave("anos_estudo_br.png", p_br,
        type = "cairo-png")
 
 ggsave("anos_estudo_reg.png", p_reg, 
+       width = 10, 
+       height = 7, 
+       units = "in", 
+       type = "cairo-png")
+
+ggsave("anos_estudo_sexo.png", p_sex, 
        width = 10, 
        height = 7, 
        units = "in", 
